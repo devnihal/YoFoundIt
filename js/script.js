b23b1ce09d115f3245e81f844fd1eb032294b58a
devnihal <mnaaksd2@gmail.com>
2025-06-11 20:07:16 +0530

    first

---
+++ js/script.js
@@ -0,0 +1,376 @@
+// let loader = ` <div id="logoContainer"><svg width="378" height="150" version="1.1" id="mainlogo" viewBox="0 0 378.5 150.79" >
+//     <path  id="logo-bubble" d="m34.497 108.77c-16.794 0-30.679-6.7989-30.679-25.114v-55.306c0-14.562 9.9642-24.527 24.527-24.527h321.8c14.562 0 24.527 9.9646 24.527 24.527v55.306c0 12.572-7.9986 24.529-24.527 24.529h-274.38l-40.681 38.785z" fill="none" stroke="#1e2f3b" stroke-linecap="round" stroke-linejoin="round" stroke-width="7.6348" style="paint-order:stroke markers fill"/>
+//     <path id="logo-text" d="m121.73 35.235-14.037 27.064v14.454h-8.3272v-14.454l-14.096-27.064h9.3978l8.9219 18.974 8.8625-18.974zm9.0609 42.052c-3.1722 0-6.0272-0.69392-8.565-2.0818-2.5378-1.4275-4.5402-3.43-6.0075-6.0075-1.4275-2.5774-2.1413-5.5514-2.1413-8.9219s0.73359-6.3445 2.2007-8.9219c1.5068-2.5774 3.5489-4.5601 6.1264-5.9479 2.5774-1.4275 5.4523-2.1413 8.6245-2.1413s6.0471 0.71376 8.6245 2.1413c2.5774 1.3878 4.5997 3.3705 6.0669 5.9479 1.5068 2.5774 2.2602 5.5514 2.2602 8.9219s-0.77323 6.3445-2.3197 8.9219c-1.5068 2.5774-3.5688 4.58-6.1859 6.0075-2.5774 1.3878-5.4722 2.0818-8.684 2.0818zm0-7.2565c1.5068 0 2.9145-0.35688 4.2231-1.0706 1.3482-0.75341 2.4188-1.8637 3.2119-3.3309 0.79306-1.4672 1.1896-3.2516 1.1896-5.3532 0-3.1326-0.83271-5.5316-2.4981-7.197-1.6258-1.7051-3.6282-2.5576-6.0075-2.5576-2.3792 0-4.3817 0.85254-6.0075 2.5576-1.5861 1.6654-2.3791 4.0644-2.3791 7.197s0.77323 5.5514 2.3197 7.2565c1.5861 1.6654 3.5688 2.4981 5.9479 2.4981zm44.912-34.796v6.7212h-17.308v10.647h13.264v6.6022h-13.264v17.546h-8.3272v-41.516zm15.327 42.052c-3.1722 0-6.0272-0.69392-8.565-2.0818-2.5378-1.4275-4.5402-3.43-6.0075-6.0075-1.4275-2.5774-2.1413-5.5514-2.1413-8.9219s0.73359-6.3445 2.2007-8.9219c1.5068-2.5774 3.5489-4.5601 6.1264-5.9479 2.5774-1.4275 5.4523-2.1413 8.6245-2.1413s6.0471 0.71376 8.6245 2.1413c2.5774 1.3878 4.5997 3.3705 6.0669 5.9479 1.5068 2.5774 2.2602 5.5514 2.2602 8.9219s-0.77324 6.3445-2.3197 8.9219c-1.5068 2.5774-3.5688 4.58-6.1859 6.0075-2.5774 1.3878-5.4722 2.0818-8.684 2.0818zm0-7.2565c1.5068 0 2.9145-0.35688 4.2231-1.0706 1.3482-0.75341 2.4188-1.8637 3.2119-3.3309 0.79304-1.4672 1.1896-3.2516 1.1896-5.3532 0-3.1326-0.83271-5.5316-2.4981-7.197-1.6258-1.7051-3.6282-2.5576-6.0075-2.5576-2.3792 0-4.3817 0.85254-6.0075 2.5576-1.5861 1.6654-2.3791 4.0644-2.3791 7.197s0.77323 5.5514 2.3197 7.2565c1.5861 1.6654 3.5688 2.4981 5.9479 2.4981zm50.961-26.23v32.951h-8.3866v-4.1635c-1.0706 1.4275-2.4783 2.5576-4.2231 3.3903-1.7051 0.79306-3.5688 1.1896-5.591 1.1896-2.5774 0-4.8575-0.53532-6.8401-1.6059-1.9827-1.1103-3.5489-2.7163-4.6988-4.8178-1.1103-2.1413-1.6654-4.6791-1.6654-7.6134v-19.33h8.3272v18.142c0 2.6171 0.65426 4.6394 1.9628 6.0669 1.3085 1.3878 3.0929 2.0818 5.3532 2.0818 2.2998 0 4.1041-0.69392 5.4126-2.0818 1.3085-1.4275 1.9628-3.4498 1.9628-6.0669v-18.142zm22.428-0.47584c3.9256 0 7.0978 1.2491 9.5167 3.7472 2.4188 2.4584 3.6282 5.9083 3.6282 10.349v19.33h-8.3272v-18.201c0-2.6171-0.65427-4.6196-1.9628-6.0075-1.3085-1.4275-3.0929-2.1413-5.3532-2.1413-2.2998 0-4.1239 0.71375-5.472 2.1413-1.3085 1.3878-1.9628 3.3903-1.9628 6.0075v18.201h-8.3272v-32.951h8.3272v4.1041c1.1103-1.4275 2.518-2.5378 4.2231-3.3309 1.7447-0.83272 3.6481-1.2491 5.71-1.2491zm16.606 16.833c0-3.3309 0.65427-6.285 1.9628-8.8625 1.3482-2.5774 3.1722-4.5601 5.4722-5.9479 2.2998-1.3878 4.8576-2.0818 7.6729-2.0818 2.1413 0 4.1834 0.47584 6.1264 1.4275 1.943 0.91202 3.4895 2.1413 4.6394 3.6878v-15.643h8.446v44.015h-8.446v-4.8773c-1.031 1.6258-2.4783 2.9343-4.342 3.9256s-4.0248 1.487-6.4832 1.487c-2.7757 0-5.3135-0.71375-7.6134-2.1413-2.3-1.4275-4.1239-3.43-5.4722-6.0075-1.3085-2.6171-1.9628-5.6109-1.9628-8.9814zm25.934 0.11895c0-2.0223-0.39653-3.7472-1.1896-5.1747-0.79307-1.4672-1.8637-2.5774-3.2119-3.3309-1.3482-0.79306-2.7956-1.1896-4.342-1.1896-1.5465 0-2.974 0.37671-4.2825 1.1301-1.3085 0.7534-2.3791 1.8637-3.2119 3.3309-0.79307 1.4275-1.1896 3.1326-1.1896 5.1153s0.39653 3.7274 1.1896 5.2342c0.8327 1.4672 1.9033 2.5973 3.2119 3.3903 1.3482 0.79306 2.7757 1.1896 4.2825 1.1896 1.5465 0 2.9938-0.37671 4.342-1.1301 1.3482-0.79306 2.4188-1.9034 3.2119-3.3309 0.79306-1.4672 1.1896-3.2119 1.1896-5.2342zm20.823-25.041v41.516h-8.3272v-41.516zm14.441 15.405v15.941c0 1.1103 0.25774 1.9231 0.77323 2.4387 0.55514 0.47584 1.4672 0.71375 2.736 0.71375h3.8662v7.0185h-5.2342c-7.0185 0-10.528-3.4102-10.528-10.23v-15.881h-3.9256v-6.8401h3.9256v-8.1487h8.3866v8.1487h7.3754v6.8401z" color="#000000" color-rendering="auto" dominant-baseline="auto" fill="#1e2f3b" image-rendering="auto" shape-rendering="auto" solid-color="#000000" stop-color="#000000" stroke-width="1.0907"  aria-label="YoFoundIt"/>
+//     <g id="logo-lens" fill="none" stroke="#1e2f3b">
+//         <path id="logo-lens-circle" d="m44.978 41.278a19.703 19.703 0 0 0 0 27.865 19.703 19.703 0 0 0 27.865 0 19.703 19.703 0 0 0 0-27.865 19.703 19.703 0 0 0-27.865 0z" stroke-width="7.6349"/>
+//         <path id="logo-lens-line" d="m44.98 69.07-18.644 19.203" stroke-linecap="round" stroke-width="7.6348"/>
+//     </g></svg></div><h1 id="loading-text">Finding Lost Items...</h1>`;
+    let Pageloaded = false;
+
+    function animate(){
+        // document.querySelector("#loading-screen").innerHTML=loader;
+
+        let t = 1;
+        setTimeout(() => {
+            document.querySelector("#logo-lens").style.transform="translateX(250px)rotateZ(-90deg)";
+            const intervlanim = setInterval(()=>{
+                if(Pageloaded){
+                    if(t==0){
+                        clearInterval(intervlanim);
+                        document.querySelector("#logo-lens").style.transform="translateX(250px)rotateZ(-90deg)";
+                        setTimeout(() => {
+                            document.querySelector("#logo-lens").style.transform="translateX(0px)rotateZ(0deg)";
+                            document.querySelector("#loading-text").style.opacity="0";
+                            document.querySelector("#mainlogo").classList.add("show");
+                            removeLoadScreen();
+                        }, 1500);
+                    }
+                    else{
+                        clearInterval(intervlanim);
+                        document.querySelector("#logo-lens").style.transform="translateX(0px)rotateZ(0deg)";
+                        setTimeout(() => {
+                            document.querySelector("#logo-lens").style.transform="translateX(250px)rotateZ(-90deg)";
+                            setTimeout(() => {
+                                document.querySelector("#loading-text").style.opacity="0";
+                                document.querySelector("#mainlogo").classList.add("show");
+                                document.querySelector("#logo-lens").style.transform="translateX(0px)rotateZ(0deg)";
+                                removeLoadScreen();
+                            }, 1500);
+                        }, 1500);
+                    }
+                }
+                if(t==1){
+                    document.querySelector("#logo-lens").style.transform="translateX(0px)rotateZ(0deg)";
+                    t=0;
+                }
+                else{
+                    t=1;
+                    document.querySelector("#logo-lens").style.transform="translateX(250px)rotateZ(-90deg)";
+                }
+            },1500)
+        }, 500);
+    }
+animate();
+
+function removeLoadScreen(){
+    setTimeout(() => {
+            let loadscreen=document.getElementById("loading-screen");
+            loadscreen.style.transition="0.5s ease-in";
+            loadscreen.style.opacity="0";
+            setTimeout(() => {
+                loadscreen.remove();
+                startUI();
+            }, 500);
+    }, 2000);
+}
+document.addEventListener("DOMContentLoaded",()=>{
+    sessionStorage.setItem("pagepath",JSON.stringify(["home"]));
+    setTimeout(() => {
+        Pageloaded=true;
+    }, 500);
+})
+
+
+
+function blurtheonthis(element){
+    let scrollTop = element.scrollTop;
+    let greeter = document.querySelector(".usergreet");
+    let hero = document.querySelector(".hero");
+    const maxScrollForEffect = 400;
+    const maxBlurPx = 5;
+
+    let normalizedScroll = Math.min(scrollTop / maxScrollForEffect, 1);
+
+
+    function easeOutQuad(t) {
+        return t * (2 - t);
+    }
+
+
+    let easedBlurValue = easeOutQuad(normalizedScroll);
+    let blurAmount = easedBlurValue * maxBlurPx;
+
+    element.style.backdropFilter = `blur(${blurAmount}px) brightness(${100 - easedBlurValue * 50}%)`;
+    greeter.style.transform = `scale(${1 - easedBlurValue * 0.1})translateY(${easedBlurValue * 50}px)`;
+    hero.style.transform = `scale(${1 - easedBlurValue * 0.1})translateY(${easedBlurValue * 50}px)`;
+
+    let boxes = document.querySelectorAll(".lfcontainer");
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top;
+        if (boxTop > window.innerHeight - 200) {
+            box.style.transform = `scale(${1 - (boxTop - (window.innerHeight - 200)) / 400})`;
+            box.style.transformOrigin="top center";
+        } else if (boxTop < 0) {
+            box.style.transform = `scale(${((Math.abs(Math.max(100 + boxTop , 0)) / 200)) + 0.5})`;
+            box.style.transformOrigin="bottom center";
+        } else {
+            box.style.transform = `scale(1)`;
+        }
+    });
+
+    let uibx = document.querySelector(".color-box-ui");
+    uibx.style.transform = `translateY(${((Math.min(scrollTop,200)/200)*10) - 10}%)`;
+}
+
+function startUI(){
+    let mainContainer = document.querySelector(".lfcontentcontainer");
+    mainContainer.style.overflowY = "scroll";
+    // const addBtn = document.querySelector('.addbtn');
+
+    // addBtn.addEventListener('click', function(event) {
+    // rippleEffect(this, event);
+    // });
+
+    // addBtn.addEventListener('touchstart', function(event) {
+    // event.preventDefault();
+    // rippleEffect(this, event);
+    // });
+
+    const boxes = document.querySelectorAll(".lfcontainer");
+    boxes.forEach(box => {
+        box.addEventListener("click", function(event) {
+            rippleEffect(this, event);
+        });
+    });
+    const nvbtns = document.querySelectorAll(".nav-button");
+    nvbtns.forEach(btn => {
+        btn.addEventListener("click",function(event){
+            // //console.log(box);
+            rippleEffect(this,event);
+        });
+    });
+}
+
+
+function rippleEffect(el, event) {
+    const ripple = document.createElement("span");
+    ripple.classList.add("ripple");
+    el.appendChild(ripple);
+
+    const elWidth = el.offsetWidth;
+    const elHeight = el.offsetHeight;
+    const size = Math.max(elWidth, elHeight);
+    let clientbndrrct = el.getBoundingClientRect();
+    let x, y;
+    if (event.type === 'touchstart' || event.type === 'touchmove') {
+        x = event.touches[0].clientX - clientbndrrct.left;
+        y = event.touches[0].clientY - clientbndrrct.top;
+    } else {
+        x = event.clientX - clientbndrrct.left;
+        y = event.clientY - clientbndrrct.top;
+    }
+
+    ripple.style.width = `${size}px`;
+    ripple.style.height = `${size}px`;
+    ripple.style.left = `${x - size / 2}px`;
+    ripple.style.top = `${y - size / 2}px`;
+
+    requestAnimationFrame(() => {
+        ripple.style.transform = `scale(1.5)`;
+        ripple.style.opacity = `0`;
+    });
+
+    ripple.addEventListener('transitionend', function handler() {
+        ripple.remove();
+        ripple.removeEventListener('transitionend', handler);
+    });
+}
+
+let navstate = true;
+
+function hidenav(){
+    const nvbar = document.querySelector(".navigation-bar");
+    nvbar.style.transform="translateX(-50%)translateY(150%)scale(1)";
+    navstate=false;
+}
+function shownav(){
+    const nvbar = document.querySelector(".navigation-bar");
+    nvbar.style.transform="translateX(-50%)translateY(0%)scale(1)";
+    navstate=true;
+}
+
+window.onclick = (event) => {
+    const navButton = event.target.closest(".nav-button");
+    if (navButton) {
+        const targetValue = navButton.dataset.target;
+        //console.log("Button clicked with data-target:", targetValue);
+        redirectTo(targetValue);
+    } else {
+        //console.log("Clicked outside .nav-button");
+    }
+};
+const nav_bg_box_pos = {
+    home:0,
+    search:20,
+    add:40,
+    profile:60,
+    settings:80
+};
+const window_names = {
+    home:"",
+    search:"windowsearch",
+    add:"windowadd",
+    profile:"windowaccount",
+    settings:80
+}
+
+function redirectTo(destination){  
+    document.body.classList.add("lockscroll");
+    // //console.log(JSON.parse(sessionStorage.getItem("pagepath")));
+    let path = JSON.parse(sessionStorage.getItem("pagepath"));
+    path.push(destination);
+    sessionStorage.setItem("pagepath",JSON.stringify(path));
+    // //console.log(path);
+    document.querySelector(".nav-active-box").style.left=nav_bg_box_pos[destination] + "%";
+    // //console.log(nav_bg_box_pos[destination]);
+    document.getElementById(window_names[destination]).classList.remove("window-hidden");
+    history.pushState({ window: window_names[destination] }, "", "");
+    // alert(history)
+    hidenav();
+}
+window.onpopstate = function(event) {
+    document.body.classList.remove("lockscroll");
+    // const windowId = event.state?.window || "homeWindow";
+    let path = JSON.parse(sessionStorage.getItem("pagepath"));
+    if(path[path.length - 1] == "search"){
+        path.pop();
+        sessionStorage.setItem("pagepath",JSON.stringify(path));
+        document.querySelector(".searchinput").value="";
+        history.replaceState(null, '', location.href);
+        closewindow("search");
+    }
+    else if(path[path.length - 1] == "add"){
+        path.pop();
+        sessionStorage.setItem("pagepath",JSON.stringify(path));
+        history.replaceState(null, '', location.href);
+        closewindow("add");
+        setTimeout(() => {
+            document.querySelector(".addform").reset();
+        }, 250);
+    }
+    else if(path[path.length - 1] == "profile"){
+        path.pop();
+        sessionStorage.setItem("pagepath",JSON.stringify(path));
+        history.replaceState(null, '', location.href);
+        if(formtype=="signup"){
+            signinform();
+        }
+        closewindow("profile");
+    }
+    if (path[path.length - 1] === "home") {
+        document.querySelector(".nav-active-box").style.left = "0%";
+
+        // 1. Replace current state with 'home'
+        history.replaceState({ window: "" }, "home", location.href);
+
+        // 2. Push a dummy state to wipe forward history
+        setTimeout(() => {
+            history.pushState({ window: "cleared" }, "", "");
+        }, 0);
+    }
+
+};
+let formtype = "login";
+function closewindow(windowname){
+    document.getElementById(window_names[windowname]).classList.add("window-hidden");
+    shownav();
+}
+
+// window.onbeforeunload = (event)=>{
+//     event.preventDefault();
+//     return false;
+// }
+document.addEventListener('keydown', function(event) {
+    if (event.keyCode === 9 || event.key === 'Tab') {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+});
+function signupform(){
+    formtype="signup";
+    let expans = document.querySelectorAll(".collapedfield");
+    expans.forEach(items =>{
+        items.classList.add("visible")
+    });
+    let title = document.querySelector(".accformtitle");
+    title.style.backgroundPosition="0% 0%";
+    setTimeout(() => {
+        title.innerHTML=`Sign Up`;
+        title.style.backgroundPosition="0% -100%";
+        // console.log("hi");
+        setTimeout(() => {
+            title.style.transition="none";
+            title.style.backgroundPosition="0% 100%";
+            setTimeout(() => {
+                title.style.transition="0.25s cubic-bezier(1, 0.04, 0.35, 0.96)";
+            }, 100);
+        }, 250);
+    }, 250);
+    
+    let btn = document.querySelector(".accbtn");
+    btn.style.color="transparent";
+    setTimeout(() => {
+        btn.textContent="Sign Up";
+        btn.style.color="var(--theme-accent)";
+    }, 250);    
+    let bttxt = document.querySelector(".accbtmtxt");
+    bttxt.style.backgroundPosition="0%";
+    setTimeout(() => {
+        bttxt.innerHTML=`<span class="accbtmtxt">Already have an account?
+        <a class="uiurl" href="javascript:void(0);" onclick="signinform()">login</a>
+        </span>`;
+        bttxt.style.backgroundPosition="-100%";
+        // console.log("hi");
+        setTimeout(() => {
+            bttxt.style.transition="none";
+            bttxt.style.backgroundPosition="100%";
+            setTimeout(() => {
+                bttxt.style.transition="0.25s cubic-bezier(1, 0.04, 0.35, 0.96)";
+            }, 100);
+        }, 250);
+    }, 250);
+}
+function signinform(){
+    formtype="login";
+    let expans = document.querySelectorAll(".collapedfield");
+    expans.forEach(items =>{
+        items.classList.remove("visible")
+    });
+    let title = document.querySelector(".accformtitle");
+    title.style.backgroundPosition="0% 0%";
+    setTimeout(() => {
+        title.innerHTML=`Login`;
+        title.style.backgroundPosition="0% -100%";
+        // console.log("hi");
+        setTimeout(() => {
+            title.style.transition="none";
+            title.style.backgroundPosition="0% 100%";
+            setTimeout(() => {
+                title.style.transition="0.25s cubic-bezier(1, 0.04, 0.35, 0.96)";
+            }, 100);
+        }, 250);
+    }, 250);
+
+    let btn = document.querySelector(".accbtn");
+    btn.style.color="transparent";
+    setTimeout(() => {
+        btn.textContent="Login";
+        btn.style.color="var(--theme-accent)";
+    }, 250);
+    let bttxt = document.querySelector(".accbtmtxt");
+    bttxt.style.backgroundPosition="0%";
+    setTimeout(() => {
+        bttxt.innerHTML=`<span class="accbtmtxt">&nbsp;Don't have an account?
+            <a class="uiurl" href="javascript:void(0);" onclick="signupform()">Sign Up</a>
+        </span>`;
+        bttxt.style.backgroundPosition="-100%";
+        // console.log("hi");
+        setTimeout(() => {
+            bttxt.style.transition="none";
+            bttxt.style.backgroundPosition="100%";
+            setTimeout(() => {
+                bttxt.style.transition="0.25s cubic-bezier(1, 0.04, 0.35, 0.96)";
+            }, 100);
+        }, 250);
+    }, 250);
+}
